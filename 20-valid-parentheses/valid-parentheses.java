0efd801d15cc1fd73bafe624ce5ef740a85186ae
Malindu Dominic Inosh Atukorala <dominic.malindu@gmail.com>
2026-06-03 23:31:06 +0530

    Time: 3 ms (88.14%) | Memory: 43.2 MB (71.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,7 +5,7 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         
         for (char c : s.toCharArray()) {
-            
+
             if (c == '(') {
                 stack.push(')');
             } else if (c == '{') {
@@ -13,9 +13,8 @@ class Solution {
             } else if (c == '[') {
                 stack.push(']');
             } 
-           
+
             else {
-              
                 if (stack.isEmpty() || stack.pop() != c) {
                     return false;
                 }
